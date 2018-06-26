6536667f7472fcb3a7c3714f0d78b5ae1d25d124
zhaoyu <774463266@qq.com>
2018-06-26 14:51:38 +0800

    添加一些对应的文字注释 ---改天可以吧数据源换成mongo试一下

--- springboot-springSecurity3/src/main/java/com/us/example/config/WebSecurityConfig.java
+++ springboot-springSecurity3/src/main/java/com/us/example/config/WebSecurityConfig.java
@@ -10,10 +10,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
 /**
  * Created by yangyibo on 17/1/18.
+ * 配置的总控 
+ *  1.  AuthenticationManagerBuilder--->>CustomUserService  
+ *      这两个配合返回用户信息 其中CustomUserService可以自定义只要实现UserDetailsService接口 
+ *      返回userdetail就可以了 包括用户的基础信息  以及权限信息
+ *  2.  configure(HttpSecurity http)
+ *      HttpSecurity 这个应该对于所有的请求的配置 springboot按照这个配置 处理所有的请求
+ *      这个方法 配置要处理的url 
+ *  3.  MyFilterSecurityInterceptor 自定义的filter 真正工作的类
+ *         MyAccessDecisionManager（AccessDecisionManager接口）这个类用来判断是否拥有权限访问
+ *         FilterInvocationSecurityMetadataSource 自定义实现这个接口 返回这个请求的是否在权限类表中 如果有返回
  */
 
 
--- springboot-springSecurity3/src/main/java/com/us/example/service/MyFilterSecurityInterceptor.java
+++ springboot-springSecurity3/src/main/java/com/us/example/service/MyFilterSecurityInterceptor.java
@@ -47,9 +47,9 @@ public class MyFilterSecurityInterceptor extends AbstractSecurityInterceptor imp
 
 
     public void invoke(FilterInvocation fi) throws IOException, ServletException {
-//fi里面有一个被拦截的url
-//里面调用MyInvocationSecurityMetadataSource的getAttributes(Object object)这个方法获取fi对应的所有权限
-//再调用MyAccessDecisionManager的decide方法来校验用户的权限是否足够
+     //fi里面有一个被拦截的url
+     //里面调用MyInvocationSecurityMetadataSource的getAttributes(Object object)这个方法获取fi对应的所有权限
+     //再调用MyAccessDecisionManager的decide方法来校验用户的权限是否足够
         InterceptorStatusToken token = super.beforeInvocation(fi);
         try {
 //执行下一个拦截器
